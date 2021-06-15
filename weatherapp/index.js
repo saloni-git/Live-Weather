386e7c7eb721ffcadc170d193f9485c634d7eecf
saloni-git <70013045+saloni-git@users.noreply.github.com>
2021-06-15 17:32:03 +0530

    Update index.js

--- weatherapp/index.js
+++ weatherapp/index.js
@@ -1,5 +1,4 @@
-//e052e23cda99505aac4836bd4ba3c781
-//api.openweathermap.org/data/2.5/weather?q=gwalior&appid=e052e23cda99505aac4836bd4ba3c781
+
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
@@ -10,13 +9,13 @@ const replaceVal = (tempval,orgval) => {
      temperature =temperature.replace("{%location%}", orgval.name);
      temperature =temperature.replace("{%country%}", orgval.sys.country);
      return temperature;
-    // res.wr
+    
 
 }
 const server = http.createServer( (req,res) => {
 if(req.url=="/")
 {
-    requests("https://api.openweathermap.org/data/2.5/weather?q=gwalior&appid=e052e23cda99505aac4836bd4ba3c781")
+    requests("https://api.openweathermap.org/data/2.5/weather?q=gwalior&appid=")
 .on('data', (chunk) => {
     const objdata = JSON.parse(chunk);
     //object to array
@@ -25,8 +24,7 @@ if(req.url=="/")
     const realdata = arrdata.map((val)=> 
         replaceVal(homefile,val)).join("");
        res.write(realdata);
-      // console.log(realdata);
-    //console.log(arrdata[0].main.temp) ;
+     
 })
 .on('end', (err) => {
   if (err) return console.log('connection closed due to errors', err);
